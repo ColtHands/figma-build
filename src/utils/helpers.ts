624d4866197af4007d526ca49cd8380c998c6d53
Aleskey.K <colthands@protonmail.com>
2023-04-01 17:41:53 +0400

    Fetching file styles/nodes/fills

---
+++ src/utils/helpers.ts
@@ -0,0 +1,13 @@
+/**
+ * This function takes node information received from `/files/:fileId/nodes` for a single node
+ */
+export function getNodeFills(){}
+
+/**
+ * This function takes all nodes received from `/files/:fileId/nodes`
+ * @returns - single node data found by it's name 
+ */
+export function getNodeByName(nodes, name) {
+    console.log(getNodeByName, nodes)
+    return nodes[Object.keys(nodes).find(key => nodes[key].document.name === name)]
+}
