dfbed3f14414f6dee83efed384c4c21dfbe3ad95
Volha Buinitskaya <vbuinitskaya@evolution.com>
2023-04-05 13:21:17 +0400

    Add color styles parsing, mock text and effect parsing

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,27 @@
 export type FigmaRestMethods = 'file' | 'files' | 'images'
+
+export enum StyleType {
+    FILL = "FILL",
+    EFFECT = "EFFECT",
+    TEXT = "TEXT",
+}
+
+export interface ColorThemeItem {
+    styleType: StyleType.FILL
+    color: string
+}
+
+export interface EffectThemeItem {
+    styleType: StyleType.EFFECT
+    // TBD
+    boxShadow: string
+}
+
+export interface TextThemeItem {
+    styleType: StyleType.TEXT
+    // TBD
+    fontSize: string
+}
+
+export type ThemeItem = ColorThemeItem | EffectThemeItem | TextThemeItem
+export type ThemeMap = Record<string, ThemeItem>
