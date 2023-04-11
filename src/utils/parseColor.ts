80a8bed7eeda0020b413c553da229508baab81bb
Volha Buinitskaya <vbuinitskaya@evolution.com>
2023-04-11 18:05:32 +0400

    Add all gradients and mixed variants support

--- src/utils/parseColor.ts
+++ src/utils/parseColor.ts
@@ -1,30 +1,62 @@
-import {Color, Fill, FillLinearGradient, FillSolid, FillType} from "../types";
+import {Color, Fill, FillGradient, FillSolid, FillType, GradientFunctionType} from "../types";
+
+const directionMap: Record<GradientFunctionType, string> = {
+    "linear-gradient": "180deg",
+    "radial-gradient": "50% 50% at 50% 50%",
+    "conic-gradient": "from 180deg at 50% 50%"
+}
 
 export function parseColorFromNode(node: { document: { fills: Fill[] } }): string {
-    const parsedFills: string[] = [];
+    const fills = node?.document?.fills ?? []
+    if (fills.length === 1 && fills[0].type === FillType.SOLID) {
+        // NOTE: simple color, other things will be parsed as gradient
+        return extractColor(fills[0])
+    }
+    const parsedFills: string[] = []
     
-    for(const fill of node?.document?.fills ?? []) {
+    for(const fill of fills ?? []) {
         switch (fill.type) {
             case FillType.SOLID:
-                parsedFills.push(extractColor(fill))
+                parsedFills.push(extractSingleColorGradient(fill))
                 break
             case FillType.GRADIENT_LINEAR:
-                parsedFills.push(extractLinearGradient(fill))
+                parsedFills.push(extractGradient(fill, "linear-gradient"))
+                break
+            case FillType.GRADIENT_RADIAL:
+            case FillType.GRADIENT_DIAMOND:
+                parsedFills.push(extractGradient(fill, "radial-gradient"))
+                break
+            case FillType.GRADIENT_ANGULAR:
+                parsedFills.push(extractGradient(fill, "conic-gradient"))
                 break
         }
+    }
+
+    const hasDifferentTypes = fills.some(fill => fills.some(f => f.type !== fill.type))
+    if (hasDifferentTypes) {
+        // NODE: mixed gradients comes in reversed order, have no idea why
+        parsedFills.reverse()
     }
     
     return parsedFills.join(", ")
 }
 
-function extractLinearGradient(fill: FillLinearGradient): string {
-    const gradientStops = fill.gradientStops.map(gradientStop => {
+function extractSingleColorGradient(fill: FillSolid): string {
+    const color = extractColor(fill);
+    return `linear-gradient(${["0deg", color, color].join(", ")})`
+}
+
+function extractGradient(fill: FillGradient, gradientFunction: GradientFunctionType): string {
+    const measureAs: "%" | "deg" = gradientFunction === "conic-gradient" ? "deg" : "%";
+    return `${gradientFunction}(${[directionMap[gradientFunction], ...extractGradientStops(fill, measureAs)].join(", ")})`
+}
+
+function extractGradientStops(fill: FillGradient, measureAs: "%" | "deg"): string[] {
+    return fill.gradientStops.map(gradientStop => {
         const color = extractColor({...gradientStop, opacity: fill.opacity})
-        const stopPointPercentage = Math.round(gradientStop.position * 100)
-        return `${color} ${stopPointPercentage}%`;
+        const stopPointPercentage = Math.round(gradientStop.position * (measureAs === "%" ? 100 : 360))
+        return `${color} ${stopPointPercentage}${measureAs}`;
     })
-    
-    return `linear-gradient(${["180deg", ...gradientStops].join(", ")})`
 }
 
 function extractColor(fill: {
