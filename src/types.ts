383b21bcbcaecf02f0fbb01900024205d03b8204
Volha Buinitskaya <vbuinitskaya@evolution.com>
2023-04-11 18:05:32 +0400

    Add all gradients and mixed variants support

--- src/types.ts
+++ src/types.ts
@@ -69,12 +69,15 @@ export interface Color {
 }
 
 export enum FillType {
+    SOLID = "SOLID",
     GRADIENT_LINEAR = "GRADIENT_LINEAR",
-    SOLID = "SOLID"
+    GRADIENT_RADIAL = "GRADIENT_RADIAL",
+    GRADIENT_ANGULAR = "GRADIENT_ANGULAR",
+    GRADIENT_DIAMOND = "GRADIENT_DIAMOND"
 }
 
-export interface FillLinearGradient {
-    type: FillType.GRADIENT_LINEAR
+export interface FillGradient {
+    type: FillType.GRADIENT_LINEAR | FillType.GRADIENT_RADIAL | FillType.GRADIENT_ANGULAR | FillType.GRADIENT_DIAMOND
     opacity: number
     gradientStops: Array<{
         color: Color;
@@ -91,4 +94,6 @@ export interface FillSolid {
     color: Color
 }
 
-export type Fill = FillLinearGradient | FillSolid
+export type Fill = FillGradient | FillSolid
+
+export type GradientFunctionType = "linear-gradient" | "radial-gradient" |"conic-gradient"
