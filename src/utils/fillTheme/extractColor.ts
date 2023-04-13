505f40541955b24943aed06fcc30ad22d0aba744
akarpenko <akarpenko@evolution.com>
2023-04-13 16:45:00 +0400

    Parse fill opacity

--- src/utils/fillTheme/extractColor.ts
+++ src/utils/fillTheme/extractColor.ts
@@ -7,9 +7,20 @@ export function extractColor(fill: {
     const color = fill.color
     const opacity = fill.opacity ?? 1
     const rgbValues = [color.r, color.g, color.b]
-    return (`rgba(${[...rgbValues.map(parseColorValue), opacity * color.a].join(", ")})`);
+    return (`rgba(${[...rgbValues.map(parseColorValue), opacity * parseAlphaOpacity(color.a)].join(", ")})`);
 }
 
 function parseColorValue(value: number): number {
     return Math.round(value * 255)
+}
+
+/**
+ * The alpha values could be any decimal point, but human eye can barely notice two decimal points of change.
+ * Even though having 0.11999999731779099 is technically correct, for readability value is rounded to two decimal points
+ * @see https://stackoverflow.com/questions/19409115/html-rgba-color-opacity
+ * @param value - 0.11999999731779099
+ * @returns - 0.12
+ */
+function parseAlphaOpacity(value: number): number {
+    return Math.round(value * 100) / 100
 }
