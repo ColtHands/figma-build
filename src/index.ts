54d4502c1e28db5d846ddc613dc3a33b09fa7223
akarpenko <akarpenko@evolution.com>
2023-04-13 11:07:24 +0400

    Split file modules

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 import { fileId, filename, outputPath, command, outputFormat } from "./arguments"
 import { Commands, OutputFormat } from './types'
-import { fetchFileData, fetchFileNodes } from './figmaApi'
 import { writeFile } from './utils/writeFile'
-import { getNodeByName } from './utils/helpers'
 import { parseColorFromNode } from "./utils/parseColor";
 import { getTextThemeStyles } from './utils/getTextTheme'
+import { getFigmaThemeStyles } from './getFigmaThemeStyles'
 import { type ColorThemeItem, type EffectThemeItem, StyleType, type ThemeMap, type TextThemeItem } from "./types";
 
 if(command === Commands.theme) {
@@ -18,51 +17,4 @@ if(command === Commands.theme) {
             writeFile('output', 'js', `module.exports = ${JSON.stringify(theme, null, 4)}`, outputPath)
         }
     })
-}
-
-async function getFigmaThemeStyles() {
-    const fileData: any = await fetchFileData(file)
-    const styleNodeIds: any = Object.keys(fileData.styles)
-    console.log('styleNodeIds', styleNodeIds)
-
-    const themeMap: ThemeMap = Object.values(fileData.styles)
-        .reduce((prev: any, values: any) => ({...prev, [values.name]: { styleType: values.styleType }}), {}) as ThemeMap;
-
-    const nodeData: any = await fetchFileNodes(fileId, styleNodeIds)
-    const nodes: any = nodeData.nodes
-    // console.log("nodes", nodes)
-
-    Object.entries(themeMap).forEach(([key, values]) => {
-        const node = getNodeByName(nodes, key)
-
-        switch (values.styleType) {
-            case StyleType.FILL:
-                if(node.document.name === "gradient") console.log("node", node.document)
-                
-                Object.assign(values, getColorThemeItem(node))
-                break
-            case StyleType.EFFECT: // shadows, blurs etc...
-                Object.assign(values, getEffectThemeItem(node))
-                break
-            case StyleType.TEXT: // text styles: font-family, font-weight, font-size, line-height, letter-spacing, paragraph-spacing, text-decorations, text-transform, etc.
-                Object.assign(values, getTextThemeStyles(node))
-                break
-        }
-    })
-    
-    return themeMap
-}
-
-function getColorThemeItem(node: any): Omit<ColorThemeItem, "styleType"> {
-    return { color: parseColorFromNode(node) }
-}
-
-function getEffectThemeItem(node: any): Omit<EffectThemeItem, "styleType"> {
-    // TODO: parse effect styles
-    return { boxShadow: "" }
-}
-
-function getTextThemeItem(node: any): Omit<TextThemeItem, "styleType"> {
-    // TODO: parse text styles
-    return { fontSize: "" }
 }
