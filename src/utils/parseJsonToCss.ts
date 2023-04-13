602208c1a2fd0a73f82df5f246e9c33bbf37ed42
akarpenko <akarpenko@evolution.com>
2023-04-13 12:50:21 +0400

    Finalizing arguments

---
+++ src/utils/parseJsonToCss.ts
@@ -0,0 +1,9 @@
+/**
+ * Takes javascript object and returns a parsed string as a valid css
+ * @param obj Javascript object
+ * @returns string as a valid css
+ */
+export function toCss(obj: any) {
+    
+    return obj
+}
