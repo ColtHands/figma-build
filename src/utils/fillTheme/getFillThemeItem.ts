826a08d44affc84fb7ef1fd7697414a22f00bcff
Volha Buinitskaya <vbuinitskaya@evolution.com>
2023-04-13 12:32:05 +0400

    Update FillTheme folder/files structure

--- src/utils/fillTheme/getFillThemeItem.ts
+++ src/utils/fillTheme/getFillThemeItem.ts
@@ -1,7 +1,15 @@
-import { type ColorThemeItem } from "../../types";
-import { parseColorFromNode } from "./parseColor";
+import { type ColorThemeItem, Fill, FillType } from "../../types";
+import { getColorThemeItem } from "./getColorThemeItem";
+import { getGradientThemeItem } from "./getGradientThemeItem";
 
-export function getFillThemeItem(node: any): Omit<ColorThemeItem, "styleType"> {
-    // TODO: devide to color and gradient type on this level
-    return { color: parseColorFromNode(node) }
+export function getFillThemeItem(node: { document: { fills: Fill[] } }): Omit<ColorThemeItem, "styleType"> {
+    if (isSingleColor(node)) {
+        return getColorThemeItem(node)
+    } 
+    return getGradientThemeItem(node)
+}
+
+const isSingleColor = (node: { document: { fills: Fill[] } }) => {
+    const fills = node?.document?.fills ?? []
+    return fills.length === 1 && fills[0].type === FillType.SOLID
 }
